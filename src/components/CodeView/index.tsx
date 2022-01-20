c06a14f05db106ca357a605688aa5fd471801354
lijunhua <lijunhua@qq.com>
2022-01-20 16:18:42 +0800

    增加代码显示组件

---
+++ src/components/CodeView/index.tsx
@@ -0,0 +1,52 @@
+import "code-prettify"
+import "code-prettify/src/prettify.css"
+
+import parserTypeScript from 'prettier/parser-typescript'
+import prettier from 'prettier/standalone'
+import parserHtml from "prettier/parser-html"
+import {Component, CSSProperties} from "react";
+import "./index.scss"
+
+export interface CodeViewProps {
+  children: string
+  className?: string
+  style?: CSSProperties
+  language?: "ts" | "html"
+}
+
+export class CodeView extends Component<CodeViewProps, {}> {
+
+  static defaultProps: CodeViewProps = {
+    children: "",
+    language: "ts"
+  }
+
+  componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<{ selectedIndex: number }>, snapshot?: any) {
+    (window as any).PR.prettyPrint();
+  }
+
+  componentDidMount() {
+    (window as any).PR.prettyPrint();
+  }
+
+  render() {
+    let result = "";
+    try {
+      result = prettier.format(this.props.children, {
+        parser: "typescript",
+        plugins: [parserTypeScript, parserHtml]
+      });
+    } catch (e) {
+      console.error("代码格式有误，无法解析")
+      result = this.props.children
+    }
+    console.log(result)
+    return (
+      <pre className={`lg-code-view prettyprint lang-js ${this.props.className || ""}`} style={this.props.style}>
+          <div className="cede_block">
+            <code className="code_text">{result}</code>
+          </div>
+      </pre>
+    )
+  }
+}
--- src/pages/breadcrumb/index.tsx
+++ src/pages/breadcrumb/index.tsx
@@ -2,7 +2,7 @@ import "./index.scss"
 import {Component} from "react";
 import {LgBreadcrumb} from "@/components/breadcrumb";
 import {AllSkinCover} from "../AllSkinCover";
-
+import {CodeView} from "@/components/CodeView";
 
 export default class BreadcrumbPage extends Component<{}, { selectedIndex: number }> {
 
@@ -13,21 +13,61 @@ export default class BreadcrumbPage extends Component<{}, { selectedIndex: numbe
     }
   }
 
+
   render() {
     return (
       <div className="lg-breadcrumb-demo-page">
-        <div className="lg-breadcrumb-demo-a">
-          <AllSkinCover>
-            <LgBreadcrumb onChange={index => this.setState({selectedIndex: index})}
-                          selectedIndex={this.state.selectedIndex}
-                          itemList={["最新选项", "最新选项", "最新选项", "最新选项", "最新选项", "最新选项"]}/>
-          </AllSkinCover>
+        <div className="component-demo">
+          <div className="lg-breadcrumb-demo-a">
+            <AllSkinCover>
+              <LgBreadcrumb onChange={index => this.setState({selectedIndex: index})}
+                            selectedIndex={this.state.selectedIndex}
+                            itemList={["最新选项", "最新选项", "最新选项", "最新选项", "最新选项", "最新选项"]}/>
+            </AllSkinCover>
+          </div>
+          <div className="lg-breadcrumb-demo-b">
+            <AllSkinCover>
+              <LgBreadcrumb type={"B"} onChange={index => this.setState({selectedIndex: index})}
+                            selectedIndex={this.state.selectedIndex} itemList={["F1", "F2", "F3", 'F4', 'F5']}/>
+            </AllSkinCover>
+          </div>
         </div>
-        <div className="lg-breadcrumb-demo-b">
-          <AllSkinCover>
-            <LgBreadcrumb type={"B"} onChange={index => this.setState({selectedIndex: index})}
-                          selectedIndex={this.state.selectedIndex} itemList={["F1", "F2", "F3", 'F4', 'F5']}/>
-          </AllSkinCover>
+        <div className="code-demo">
+          <div>在页面文件中引入组件</div>
+          <CodeView className="code-size">
+            {`
+               import {LgBreadcrumb} from "@/components/breadcrumb";
+            `}
+          </CodeView>
+
+          <div>标签属性详解</div>
+          <CodeView className="code-size">
+            {`interface LgBreadcrumbProps{
+               type?:"A" | "B" // 款式选择，"A"、"B",可空，默认为A
+               itemList:string[] // 要显示的文字
+               selectedIndex?:number //设置当前选中的index,可空，默认为0
+               onChange?:(index: number) => void // 点击是触发，需要通过这个index参数修改selectedIndex，可空sfdsadasdfsfasdf
+               }
+            `}
+          </CodeView>
+
+          <div>A款锚点链接 样例代码</div>
+          <CodeView language="html" className="code-size">
+            {`
+              <LgBreadcrumb onChange={index => this.setState({selectedIndex: index})}
+                            selectedIndex={this.state.selectedIndex}
+                            itemList={["最新选项", "最新选项", "最新选项", "最新选项", "最新选项", "最新选项"]}/>
+            `}
+          </CodeView>
+
+          <div>B款指定楼层 样例代码</div>
+          <CodeView language="html" className="code-size">
+            {`
+              <LgBreadcrumb type={"B"} onChange={index => this.setState({selectedIndex: index})}
+                            selectedIndex={this.state.selectedIndex} itemList={["F1", "F2", "F3", 'F4', 'F5']}/>
+            `}
+          </CodeView>
+
         </div>
       </div>
     )
