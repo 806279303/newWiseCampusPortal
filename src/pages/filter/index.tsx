4ca37df408662bc922cce674b21973ab06c06b84
zhongli <271268970@qq.com>
2022-01-21 10:48:24 +0800

    完善筛选器

--- src/pages/filter/index.tsx
+++ src/pages/filter/index.tsx
@@ -1,14 +1,38 @@
 import {Component} from "react";
 import {allSkinClassName} from "@/components/index";
 import {Filter} from "@/components/filter";
+
+let filterConfig = [{
+  key: "nameA",
+  name: "选项A",
+  disable: false
+},{
+  key: "valueB",
+  name: "选项B",
+},{
+  key: "typec",
+  name: "选项C",
+ 
+},{
+  key: "typed",
+  name: "选项D",
+
+}]
+
+let checkValue = ["typec"]
+
+function onChange(e: string[]){
+  console.log(e)
+}
+
 export default () => {
     return (
         <div>
         {
           allSkinClassName.map((className,index) => {
             return (
-              <div className={className}>
-                <Filter/>
+              <div className={className} key={index}>
+                <Filter type="radio" filterConfig={filterConfig} checkValue={checkValue} onChange={onChange}/>
               </div>
             )
           })
