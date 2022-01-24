c25dd807836774c5671a62c1c578bf9171405e61
moguangjiu <1379387303@qq.com>
2022-01-24 09:06:12 +0800

    添加时间线、折叠版、步骤条、抽屉说明

--- src/components/collapse/index.tsx
+++ src/components/collapse/index.tsx
@@ -4,45 +4,15 @@ import { Collapse } from 'element-react'
 interface Props {
     className?: string,
     style?: object,
-    type: 'A' | 'B' | 'C',
     list: any,
-    accordion?: boolean//嵌套模板是否展示多个，默认多个个展示(true:展示单个，false：展示多个)
+    accordion?: boolean//是否展示多个，默认多个个展示(true:展示单个，false：展示多个)
 
 }
 const LgCollapse = (props: Props): any => {
-    if (props.type == "A" || props.type == "B")
-        return <CollapseAB {...props} />
-    else if (props.type == "C")
         return <CollapseC {...props} />
 }
 
-function CollapseAB(props: Props) {
-    return (
-        <div className={`collapse_content ${props.className}`} style={props.style || {}}>
-            <Collapse value="" accordion={props.type == 'A' ? false : true}>
-                {
-                    props.list.map((item: any, index: any) => {
-                        var disable=item.disable?"disable":""
-                        return (
-                            <Collapse.Item    key={"A" + index} title={<span className={disable} onClick={(e) => { if(item.disable) e.stopPropagation() }}>{item.title} <span className='disable_span'></span></span> } name={index.toString()}>
-                                {
-                                    item.list.map((o: any, i: any) => {
-                                        return (
-                                            <div key={"a" + i}>
-                                                {o.content}
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </Collapse.Item>
 
-                        )
-                    })
-                }
-            </Collapse>
-        </div>
-    )
-}
 function CollapseC(props: Props) {
     console.log(props.accordion)
     return (
@@ -51,17 +21,18 @@ function CollapseC(props: Props) {
                 {
                     props.list.map((item: any, index: any) => {
                         //console.log(item)
-                        var disable=item.disable?"disable":""
+                        var disable = item.disable ? "disable" : ""
                         return (
-                            <Collapse.Item  key={"C" + index} title={<span className={disable} onClick={(e) => { if(item.disable) e.stopPropagation() }}>{item.title} <span className='disable_span'></span></span> } name={index.toString()}>
+                            <Collapse.Item key={"C" + index} title={<span className={disable} onClick={(e) => { if (item.disable) e.stopPropagation() }}>{item.title} <span className='disable_span'></span></span>} name={index.toString()}>
                                 {
                                     item.list[0].list && item.list[0].list.length > 0 ?
                                         <Collapse value="" accordion={props.accordion}>
                                             {
                                                 item.list.map((item2: any, index2: any) => {
-                                                    var disable1=item.disable?"disable":""
+                                                    var disable1 = item2.disable ? "disable" : ""
+                                                   // console.log(item2)
                                                     return (
-                                                        <Collapse.Item key={"c" + index2} title={<span className={disable1} onClick={(e) => {if(item.disable) e.stopPropagation() }}>{item2.title} <span className='disable_span'></span></span> } name={index2.toString()}>
+                                                        <Collapse.Item key={"c" + index2} title={<span className={disable1} onClick={(e) => { if (item2.disable) e.stopPropagation() }}>{item2.title} <span className='disable_span'></span></span>} name={index2.toString()}>
                                                             {
                                                                 item2.list.map((item3: any, index3: any) => {
                                                                     return (
