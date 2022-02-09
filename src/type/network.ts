2b178523e7dc7db3b141cf50d8fa047ebaf2b79b
Jack <jack@qq.com>
2022-02-09 14:20:18 +0800

    ‘update’

--- src/type/network.ts
+++ src/type/network.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BaseUrl = process.env.NODE_ENV === "development" ? "http://192.168.129.247:15666/car/" : "/";//根据各自项目的服务器地址填写
 
+axios.defaults.baseURL = BaseUrl;
 // export const 
 
 
