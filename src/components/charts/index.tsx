7f8aa0223a7e2eaddab5c06fff5d7676ba1fe0b2
bowei <806279303@qq.com>
2022-02-09 13:45:31 +0800

    更新代码

--- src/components/charts/index.tsx
+++ src/components/charts/index.tsx
@@ -2,15 +2,8 @@ import { Component } from "react";
 import "./index.scss";
 import ReactEcharts from 'echarts-for-react'
 import { EChartsReactProps, EChartsOption, EChartsInstance } from "echarts-for-react";
-import * as echarts from 'echarts';
-
-export const LgChartsClass = echarts;
-
 
 export class LgCharts extends Component<EChartsReactProps, {}>{
-    
-    static readonly LgChartsClass = echarts
-
     constructor(props: EChartsReactProps) {
         super(props)
     }
