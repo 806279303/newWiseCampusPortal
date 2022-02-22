c54af64a740d638b701ef4d13e0f0e8b2d105632
lijunhua <lijunhua@qq.com>
2022-02-22 15:58:06 +0800

    日期选择demo以修改

--- src/pages/datePicker/index.tsx
+++ src/pages/datePicker/index.tsx
@@ -3,7 +3,8 @@ import {LgDatePicker} from "@/components/datePicker";
 import "./index.scss"
 import {allSkinClassName} from "@/components/index";
 import {BaseComponent} from "../../type/BaseComponent";
-import {CodeView} from "@/components/CodeView";
+import {DemoPage} from "../demoPage";
+import {DemoView} from "@/components/demoView";
 
 
 type DateType = Date | string | null
@@ -47,7 +48,7 @@ export default class DatePicker extends Component<{}, DatePickerState> {
     this.setState({startTime})
   }
 
-  handleEndUpdate(endTime: any){
+  handleEndUpdate(endTime: any) {
     console.debug('time-select endDate update: ', endTime)
     this.setState({endTime})
   }
@@ -56,20 +57,13 @@ export default class DatePicker extends Component<{}, DatePickerState> {
   render() {
     const end = new Date();
     const start = new Date();
-    start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+    start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
     return (
-      <div className="lg-date-picker-demo">
-        <div>在页面文件中引入组件</div>
-        <CodeView className="code-size">
-          {`
-              import {LgDatePicker} from "@/components/datePicker";
-          `}
-        </CodeView>
-
-
-        <div>标签属性详解</div>
-        <CodeView className="code-size">
-          {`
+      <DemoPage title="G018时间/日期选择器" subtitle="常用锚点定位" className="lg-date-picker-demo"
+                importCode={`
+                    import {LgDatePicker} from "@/components/datePicker";
+                `}
+                interfaceCode={`
             /* dateType可以是Date或者字符串 */
             type dateType = Date | string | null;
             interface LgDatePickerProps{
@@ -104,12 +98,8 @@ export default class DatePicker extends Component<{}, DatePickerState> {
               text: string
               onClick(): void
             }
-          `}
-        </CodeView>
-
-        <div>A款选择日样例</div>
-        <CodeView className="code-size">
-          {`
+          `}>
+        <DemoView title="A款日期选择" code={`
             <>
               <LgDatePicker onChange={e => console.log(e?.getTime())}/>
               <LgDatePicker onChange={e => console.log(e)} onRef={e => this.demonstrationPicker = e} align={"right"}
@@ -140,149 +130,169 @@ export default class DatePicker extends Component<{}, DatePickerState> {
                               }
                             ]}/>
             </>
-          `}
-        </CodeView>
-        <DatePickerDemoFrame className="selected-day" title="选择日" subtitle="以「日」为基本单位，基础的日期选择控件">
-          <div className="left">
-            <div className="component-title">默认</div>
-            <LgDatePicker placeholder="请选择" onChange={e => console.log(e?.getTime())}/>
+        `}>
+          <div className="lg-date-picker-demo-h">
+            <div className="lg-date-picker-demo-v">
+              <div className="component-title">默认</div>
+              <LgDatePicker placeholder="请选择" onChange={e => console.log(e?.getTime())}/>
+            </div>
+            <div className="lg-date-picker-demo-v">
+              <div className="component-title">带快捷选项</div>
+              <LgDatePicker placeholder="请选择" onChange={e => console.log(e)} onRef={e => this.demonstrationPicker = e}
+                            align={"right"}
+                            value={this.state.demonstrationValue}
+                            shortcuts={[
+                              {
+                                text: '今天',
+                                onClick: () => {
+                                  this.demonstrationPicker.togglePickerVisible()
+                                  this.setState({demonstrationValue: new Date()})
+                                }
+                              }, {
+                                text: '昨天',
+                                onClick: () => {
+                                  const date = new Date();
+                                  date.setTime(date.getTime() - 3600 * 1000 * 24);
+                                  this.demonstrationPicker.togglePickerVisible()
+                                  this.setState({demonstrationValue: date})
+                                }
+                              }, {
+                                text: '一周前',
+                                onClick: () => {
+                                  const date = new Date();
+                                  date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                                  this.demonstrationPicker.togglePickerVisible()
+                                  this.setState({demonstrationValue: date})
+                                }
+                              }
+                            ]}/>
+            </div>
           </div>
-          <div className="right">
-            <div className="component-title">带快捷选项</div>
-            <LgDatePicker placeholder="请选择" onChange={e => console.log(e)} onRef={e => this.demonstrationPicker = e} align={"right"}
-                          value={this.state.demonstrationValue}
-                          shortcuts={[
-                            {
-                              text: '今天',
+        </DemoView>
+
+        <DemoView title="B款选择日期范围" code={`
+              <>
+                <LgDatePicker type="B" value={[start, end]} onChange={e => console.log(e)}/>
+                <LgDatePicker type="B" onChange={e => console.log(e)} onRef={e => this.demonstrationPicker2 = e}
+                            align={"right"} value={this.state.demonstrationValue2}
+                            shortcuts={[{
+                              text: '最近一周',
                               onClick: () => {
-                                this.demonstrationPicker.togglePickerVisible()
-                                this.setState({demonstrationValue: new Date()})
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+  
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
                               }
                             }, {
-                              text: '昨天',
+                              text: '最近一个月',
                               onClick: () => {
-                                const date = new Date();
-                                date.setTime(date.getTime() - 3600 * 1000 * 24);
-                                this.demonstrationPicker.togglePickerVisible()
-                                this.setState({demonstrationValue: date})
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+  
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
                               }
                             }, {
-                              text: '一周前',
+                              text: '最近三个月',
                               onClick: () => {
-                                const date = new Date();
-                                date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-                                this.demonstrationPicker.togglePickerVisible()
-                                this.setState({demonstrationValue: date})
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
                               }
-                            }
-                          ]}/>
-          </div>
-        </DatePickerDemoFrame>
-
-
-        <div style={{paddingTop: "10px"}}>B款选择日期范围样例</div>
-        <CodeView className="code-size">
-          {`
-            <>
-              <LgDatePicker type="B" value={[start, end]} onChange={e => console.log(e)}/>
-              <LgDatePicker type="B" onChange={e => console.log(e)} onRef={e => this.demonstrationPicker2 = e}
-                          align={"right"} value={this.state.demonstrationValue2}
-                          shortcuts={[{
-                            text: '最近一周',
-                            onClick: () => {
-                              const end = new Date();
-                              const start = new Date();
-                              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-
-                              this.setState({demonstrationValue2: [start, end]})
-                              this.demonstrationPicker2.togglePickerVisible()
-                            }
-                          }, {
-                            text: '最近一个月',
-                            onClick: () => {
-                              const end = new Date();
-                              const start = new Date();
-                              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-
-                              this.setState({demonstrationValue2: [start, end]})
-                              this.demonstrationPicker2.togglePickerVisible()
-                            }
-                          }, {
-                            text: '最近三个月',
-                            onClick: () => {
-                              const end = new Date();
-                              const start = new Date();
-                              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-                              this.setState({demonstrationValue2: [start, end]})
-                              this.demonstrationPicker2.togglePickerVisible()
-                            }
-                          }]}
-            />
+                            }]}
+              />
             </>
-          `}
-        </CodeView>
+        `}>
+          <div className="lg-date-picker-demo-h">
+            <div className="lg-date-picker-demo-v">
+              <div className="component-title">默认</div>
+              <LgDatePicker type="B" value={[start, end]} onChange={e => console.log(e)}/>
+            </div>
+            <div className="lg-date-picker-demo-v">
+              <div className="component-title">带快捷选项</div>
+              <LgDatePicker type="B" onChange={e => console.log(e)} onRef={e => this.demonstrationPicker2 = e}
+                            align={"right"} value={this.state.demonstrationValue2}
+                            shortcuts={[{
+                              text: '最近一周',
+                              onClick: () => {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
 
-        <DatePickerDemoFrame className="selected-day" title="选择日期范围" subtitle="可在一个选择器中便捷地选择一个时间范围">
-          <div className="left">
-            <div className="component-title">默认</div>
-            <LgDatePicker type="B" value={[start, end]} onChange={e => console.log(e)}/>
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
+                              }
+                            }, {
+                              text: '最近一个月',
+                              onClick: () => {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
+                              }
+                            }, {
+                              text: '最近三个月',
+                              onClick: () => {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
+                              }
+                            }]}
+              />
+            </div>
           </div>
-          <div className="right">
-            <div className="component-title">带快捷选项</div>
-            <LgDatePicker type="B" onChange={e => console.log(e)} onRef={e => this.demonstrationPicker2 = e}
-                          align={"right"} value={this.state.demonstrationValue2}
-                          shortcuts={[{
-                            text: '最近一周',
-                            onClick: () => {
-                              const end = new Date();
-                              const start = new Date();
-                              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-
-                              this.setState({demonstrationValue2: [start, end]})
-                              this.demonstrationPicker2.togglePickerVisible()
-                            }
-                          }, {
-                            text: '最近一个月',
-                            onClick: () => {
-                              const end = new Date();
-                              const start = new Date();
-                              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-
-                              this.setState({demonstrationValue2: [start, end]})
-                              this.demonstrationPicker2.togglePickerVisible()
-                            }
-                          }, {
-                            text: '最近三个月',
-                            onClick: () => {
-                              const end = new Date();
-                              const start = new Date();
-                              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-                              this.setState({demonstrationValue2: [start, end]})
-                              this.demonstrationPicker2.togglePickerVisible()
-                            }
-                          }]}
-            />
-          </div>
-        </DatePickerDemoFrame>
-
-        <div style={{paddingTop: "10px"}}>C款固定时间点样例</div>
-        <CodeView className="code-size">
-          {`
-            <LgDatePicker type="C" start="08:30" step="00:15" end="18:30" maxTime="12:30"
-                          value={new Date(2016, 9, 10, 8, 30)} onChange={e => console.log(e)}/>
-          `}
-        </CodeView>
-
-        <DatePickerDemoFrame className="selected-time" title="固定时间点" subtitle="提供几个固定的时间点供用户选择">
-          <div className="left">
-            <LgDatePicker type="C" start="08:30" step="00:15" end="18:30" maxTime="12:30"
-                          value={new Date(2016, 9, 10, 8, 30)} onChange={e => console.log(e)}/>
-          </div>
-        </DatePickerDemoFrame>
-
-        <div style={{paddingTop: "10px"}}>D款固定时间范围样例</div>
-        <CodeView className="code-size">
-          {`
+        </DemoView>
+        <DemoView title="C款固定时间点" code={`
+              <>
+                <LgDatePicker type="B" value={[start, end]} onChange={e => console.log(e)}/>
+                <LgDatePicker type="B" onChange={e => console.log(e)} onRef={e => this.demonstrationPicker2 = e}
+                            align={"right"} value={this.state.demonstrationValue2}
+                            shortcuts={[{
+                              text: '最近一周',
+                              onClick: () => {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+  
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
+                              }
+                            }, {
+                              text: '最近一个月',
+                              onClick: () => {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+  
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
+                              }
+                            }, {
+                              text: '最近三个月',
+                              onClick: () => {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                                this.setState({demonstrationValue2: [start, end]})
+                                this.demonstrationPicker2.togglePickerVisible()
+                              }
+                            }]}
+              />
+            </>
+        `}>
+          <LgDatePicker type="C" start="08:30" step="00:15" end="18:30" maxTime="12:30"
+                        value={new Date(2016, 9, 10, 8, 30)} onChange={e => console.log(e)}/>
+        </DemoView>
+        <DemoView title="D款固定时间范围" code={`
               function handleStartUpdate(startTime: any) {
               console.debug('time-select startDate update: ', startTime)
               this.setState({startTime})
@@ -298,42 +308,17 @@ export default class DatePicker extends Component<{}, DatePickerState> {
             <LgDatePicker type="C" start="08:30" step="00:15" end="18:30" maxTime="12:30"
                           value={this.state.endTime} onChange={this.handleEndUpdate.bind(this)} minTime={this.state.startTime}/>
             </>
-          `}
-        </CodeView>
-
-        <DatePickerDemoFrame className="selected-range-time" title="固定时间范围" subtitle="若先选择开始时间，则结束时间内备选项的状态会随之改变">
-          <div className="left">
+          `}>
+          <div style={{display: "flex"}}>
             <LgDatePicker type="C" start="08:30" step="00:15" end="18:30" maxTime="12:30"
                           value={this.state.startTime} onChange={this.handleStartUpdate.bind(this)}/>
+            <div style={{width: "10px"}}/>
             <LgDatePicker type="C" start="08:30" step="00:15" end="18:30" maxTime="12:30"
-                          value={this.state.endTime} onChange={this.handleEndUpdate.bind(this)} minTime={this.state.startTime}/>
+                          value={this.state.endTime} onChange={this.handleEndUpdate.bind(this)}
+                          minTime={this.state.startTime}/>
           </div>
-        </DatePickerDemoFrame>
-
-        <div style={{height: "20px"}} />
-      </div>
-    )
-  }
-}
-
-
-interface DatePickerDemoFrameProps {
-  title: string
-  subtitle: string
-}
-
-class DatePickerDemoFrame extends BaseComponent<DatePickerDemoFrameProps> {
-  render() {
-    return (
-      <div className={`lg-date-picker-demo-frame ${this.props.className || ""}`}>
-        <div className="title">{this.props.title}</div>
-        <div className="subtitle">{this.props.subtitle}</div>
-        <div className="component-container">
-          {
-            this.props.children
-          }
-        </div>
-      </div>
+        </DemoView>
+      </DemoPage>
     )
   }
 }
