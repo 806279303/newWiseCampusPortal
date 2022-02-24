eaee0896bffd5c1979f9a42ffc943ff0fdc79378
zhongli <271268970@qq.com>
2022-02-24 11:16:09 +0800

    完善筛选功能

--- src/pages/filter/index.tsx
+++ src/pages/filter/index.tsx
@@ -4,6 +4,9 @@ import {Filter} from "@/components/filter";
 import {CodeView} from "@/components/CodeView";
 import TreeSelectComponent from '../treeselect';
 
+import {DemoView} from "@/components/demoView";
+import {DemoPage} from "../demoPage";
+
 
 let filterConfig = [{
   key: "nameA",
@@ -31,75 +34,64 @@ function onChange(e: string[]){
 export default () => {
     return (
         <div>
-        <div>筛选</div>
-        <div>在页面文件中引入组件</div>
-        <CodeView className="code-size">
-            {`
-                import {Filter} from "@/components/filter";
-            `}
-        </CodeView>
-        <CodeView className="code-size">
-          {`interface LgLoadingProps{
-              title?: string, //筛选前的提示文字
-              filterConfig: object[]//[{key:"字段名",name:"按键名称"},...]key必须唯一
-              checkValue?: string[]//选中的字段，和key一致的时候视为选中
-              className?: string,
-              style?: object,
-              onChange?: (value: string[]) => void//激活时回调，显示当前激活的key值
-            }
-          `}
-        </CodeView>
-        <div>代码使用示例</div>
-        <CodeView className="code-size">
-          {`
-          import {Filter} from "@/components/filter";
-            let filterConfig = [{
-              key: "nameA",
-              name: "选项A"
-            },{
-              key: "valueB",
-              name: "选项B"
-            },{
-              key: "typec",
-              name: "选项C
-            },{
-              key: "typed",
-              name: "选项D"
-            }]
+            <DemoPage title="G012筛选" subtitle="用于筛选信息"
+                importCode={`
+                    import {Filter} from "@/components/filter";
+                `}
+                interfaceCode={`
+                    interface FilterProps{
+                      type?: "radio" | "default" //单选或者多选
+                      title?: string, //筛选前的提示文字
+                      filterConfig: object[]//[{key:"字段名",name:"按键名称"},...]key必须唯一
+                      checkValue?: string[]//选中的字段，和key一致的时候视为选中
+                      className?: string,
+                      style?: object,
+                      onChange?: (value: string[]) => void//激活时回调，显示当前激活的key值
+                    }
+                `}>
 
-            let checkValue = ["typec"]
+                <DemoView title="列表型号资料检索" code={`
+                    import {Filter} from "@/components/filter";
+                    let filterConfig = [{
+                      key: "nameA",
+                      name: "选项A"
+                    },{
+                      key: "valueB",
+                      name: "选项B"
+                    },{
+                      key: "typec",
+                      name: "选项C
+                    },{
+                      key: "typed",
+                      name: "选项D"
+                    }]
 
-            function onChange(e: string[]){
-              console.log(e)
-            }
+                    let checkValue = ["typec"]
 
-            function SearchComponent(){
-                function onSearch(e){
-                    console.log(e)
-                }
-                return (
+                    function onChange(e: string[]){
+                      console.log(e)
+                    }
+
+                    function SearchComponent(){
+                        function onSearch(e){
+                            console.log(e)
+                        }
+                        return (
+                          <div>
+                              <Filter type="radio" 
+                                      filterConfig={filterConfig} 
+                                      checkValue={checkValue} 
+                                      onChange={onChange}/>
+                            </div>
+                        )
+                    }
+                `}>
                   <div>
-                      <Filter type="radio" 
-                              filterConfig={filterConfig} 
-                              checkValue={checkValue} 
-                              onChange={onChange}/>
-                    </div>
-                )
-            }
-            export default SearchComponent
-            
-          `}
-        </CodeView>
-        {
-          allSkinClassName.map((className,index) => {
-            return (
-              <div className={className} key={index}>
-                <Filter type="radio" title={"筛选 :"} filterConfig={filterConfig} checkValue={checkValue} onChange={onChange}/>
-              </div>
-            )
-          })
-        }
-        <TreeSelectComponent />
-      </div>
+                    <Filter type="radio" title={"筛选 :"} filterConfig={filterConfig} checkValue={checkValue} onChange={onChange}/>
+                  </div>
+                </DemoView>              
+            </DemoPage>
+            <TreeSelectComponent />
+        </div>
     )
 }
